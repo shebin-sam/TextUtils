7d2afcfbfdd3671e86e20d75d72c30a78a55549f
shebin-sam <shebinsam22@karunya.edu.in>
2023-05-14 14:09:53 +0530

    iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
 
-
+} from "react-router-dom";
 function App() {
   const[mode,setdarkmode]=useState('light')
 
@@ -10,7 +15,16 @@ function App() {
     if (mode ==='light'){
     setdarkmode('dark')
     document.body.style.backgroundColor ='grey';
+    document.title = "Text Utils";
+    //blinking title 
+    //setInterval(()=>{
+     // document.title = "Text";
+    //},2000);
+    //setInterval(()=>{
+      //document.title = "Utils";
+    //},2000);
   }
+
    else{
     setdarkmode('light')
     document.body.style.backgroundColor ='white';
@@ -18,12 +32,16 @@ function App() {
 }
   return (
    <>
-  
+
+
+<Router> 
 <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-<div className="container"><TextForm heading ="Enter The Text To Analyze"/>
-
-</div>
-
+<Routes>   
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" />} />
+          <Route exact path="about" element={<About />} />
+          
+</Routes>
+</Router> 
    </>
     
     
